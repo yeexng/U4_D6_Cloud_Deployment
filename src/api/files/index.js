fb322901e6989b849e8171315d4bc83e5cf506b2
yeexng <118996751+yeexng@users.noreply.github.com>
2023-03-09 01:22:49 +0000

    Update cloudinary

--- src/api/files/index.js
+++ src/api/files/index.js
@@ -1,22 +1,50 @@
 import Express from "express";
 import multer from "multer";
 import { pipeline } from "stream";
-import { getBlogPosts, saveCoverImage } from "../../lib/fs-tools.js";
+import {
+  getBlogPosts,
+  saveCoverImage,
+  writeBlogPosts,
+} from "../../lib/fs-tools.js";
 import { extname } from "path";
 import { blogPostToPDFReadableStream } from "../../lib/pdf-tools.js";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 
 const filesRouter = Express.Router();
+const cloudinaryUploader = multer({
+  storage: new CloudinaryStorage({
+    cloudinary,
+    params: { folder: "BlogPosts/covers" },
+  }),
+}).single("uploadCover");
 
 filesRouter.post(
   "/:id/uploadCover",
-  multer().single("uploadCover"),
+  // multer().single("uploadCover"),
+  cloudinaryUploader,
   async (req, res, next) => {
     //uploadCover here must be match with the FE and postman when upload... (link)
     try {
+      const allBlogPosts = await getBlogPosts();
+      const blogPostsId = req.params.id;
+      const index = allBlogPosts.findIndex((b) => b.id === blogPostsId);
+      if (index !== -1) {
+        const oldPost = allBlogPosts[index];
+        const updatedPost = {
+          ...oldPost,
+          ...req.body,
+          cover: req.file.path,
+          updatedAt: new Date(),
+        };
+      }
+      allBlogPosts[index] = updatedPost;
+      await writeBlogPosts(allBlogPosts);
       console.log("FILE:", req.file);
-      const originalFileExtension = extname(req.file.originalname);
-      const fileName = req.params.id + originalFileExtension; // using the same "id" name from link/ router
-      await saveCoverImage(fileName, req.file.buffer);
+      // const originalFileExtension = extname(req.file.originalname);
+      // const fileName = req.params.id + originalFileExtension; // using the same "id" name from link/ router
+      // await saveCoverImage(fileName, req.file.buffer);
+
       res.send({ message: "file uploaded" });
     } catch (error) {
       next(error);
@@ -34,25 +62,6 @@ filesRouter.post(
 
 // filesRouter.post("/uploadAvatar", multer(), async (req, res, next) => {
 //   try {
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// filesRouter.get("/:blogPostId/pdf", async (req, res, next) => {
-//   try {
-//     const blogPosts = await getBlogPosts();
-//     const blogPostWithID = blogPosts.find(
-//       (b) => b.id === req.params.blogPostId
-//     );
-//     if (blogPostWithID) {
-//       res.setHeader("Content-Disposition", "attachment; filename=example.pdf");
-//       const source = getPDFJSONReadableStream(blogPostWithID);
-//       const destination = res;
-//       pipeline(source, destination, (err) => {
-//         if (err) console.log(err);
-//       });
-//     }
 //   } catch (error) {
 //     next(error);
 //   }
